addb9908412139c7418c1009502e07aa6e86f871
j-belec <jeronimobelec@hotmail.com>
2023-01-06 11:34:19 -0300

    agrego carpeta public y views, app.js, detalle-producto y .gitignore

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+
+app.get("/detalle-producto", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/detalle-producto.html"));
+});
